47e8ffff565eea1e4ca69ca4d7b4e4499080bd7d
Angelina Liapin <angelina.liapin@gmail.com>
2023-04-04 16:40:40 +0300

    feat: add popup and style for burger

--- shelter/main/index.js
+++ shelter/main/index.js
@@ -1,1 +1,34 @@
 console.log('Все пункты выполнены (100/100) \nВёрстка страницы Main соответствует макету при ширине экрана 1280px: +14 \n Вёрстка страницы Main соответствует макету при ширине экрана 768px: +14  \nВёрстка страницы Main соответствует макету при ширине экрана 320px: +14\n Вёрстка страницы Pets соответствует макету при ширине экрана 1280px: +6\n Вёрстка страницы Pets соответствует макету при ширине экрана 768px: +6 \n 6Вёрстка страницы Pets соответствует макету при ширине экрана 320px: +6 \nНи на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки, справа от отдельных блоков не появляются белые поля. Весь контент страницы при этом сохраняется: не обрезается и не удаляется: +20\n Верстка резиновая: при плавном изменении размера экрана от 1280px до 320px верстка подстраивается под этот размер, элементы верстки меняют свои размеры и расположение, не наезжают друг на друга, изображения могут менять размер, но сохраняют правильные пропорции (Примеры неправильной и правильной реализации): +8\n При ширине экрана меньше 768px на обеих страницах меню в хедере скрывается, появляется иконка бургер-меню: +4\n Верстка обеих страниц валидная: для проверки валидности вёрстки используйте сервис https://validator.w3.org/ : +8') 
+ 
+const card = document.querySelectorAll('.card');
+const popup = document.querySelector('.popap-our-friends');
+const content = document.querySelector('.content-popap');
+const closePopap = document.querySelector('.close-popap');
+ 
+/*Popup*/
+
+card.forEach((div) => {
+    div.addEventListener('click', (e) => {
+        e.preventDefault();
+        popup.classList.add('active');
+        document.documentElement.style.overflow = 'hidden'; 
+    });
+});
+
+closePopap.addEventListener('click', () => {
+    popup.classList.remove('active');
+    document.documentElement.style.overflow = 'auto'; 
+});
+
+popup.addEventListener('click', () => {
+    popup.classList.remove('active');
+    document.documentElement.style.overflow = 'auto'; 
+});
+
+content.addEventListener('click', (event) => {
+   event.stopPropagation();
+});
+
+/*Burger*/
+
+
