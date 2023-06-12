18af43ba78c5cc054a49c4a970a92506ddc5baa7
Angelina Liapin <angelina.liapin@gmail.com>
2023-06-12 19:36:43 +0300

    fix: fix eslint error

--- migration-ts/src/components/controller/appLoader.ts
+++ migration-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '82e15d18d1a54f738fe3b52d2b6d9d57', // получите свой ключ https://newsapi.org/
         });
     }
